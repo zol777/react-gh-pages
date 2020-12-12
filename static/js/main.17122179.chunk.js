1d4aca36685d3b1a4c94cef8630f05d2c2d5e484
zol777 <zol777@gmail.com>
2020-12-12 18:50:20 +0800

    Updates

--- static/js/main.17122179.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-gh-pages"]=this["webpackJsonpreact-gh-pages"]||[]).push([[0],{13:function(e,t,r){},18:function(e,t,r){"use strict";r.r(t);var n=r(4),c=(r(0),r(3)),a=r.n(c),o=(r(13),r(32));function s(){return Object(n.jsx)(o.a,{variant:"contained",color:"primary",children:"\u4f60\u597d\uff0c\u4e16\u754c"})}a.a.render(Object(n.jsx)(s,{}),document.querySelector("#app"));var i=s;a.a.render(Object(n.jsx)(i,{}),document.getElementById("root"))}},[[18,1,2]]]);
-//# sourceMappingURL=main.17122179.chunk.js.map
---
+++ static/js/main.ee828ad5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-gh-pages"]=this["webpackJsonpreact-gh-pages"]||[]).push([[0],{13:function(e,r,t){},18:function(e,r,t){"use strict";t.r(r);var n=t(4),c=(t(0),t(3)),a=t.n(c),o=(t(13),t(32));function s(){return Object(n.jsx)(o.a,{variant:"contained",color:"primary",children:"\u4f60\u597d\uff0c\u4e16\u754c"})}a.a.render(Object(n.jsx)(s,{}),document.querySelector("#app"));a.a.render(Object(n.jsx)("h1",{children:"Hello World"}),document.getElementById("root"))}},[[18,1,2]]]);
+//# sourceMappingURL=main.ee828ad5.chunk.js.map
