8965851b0acf831a00f7653a162319fcc535232a
zol777 <zol777@gmail.com>
2020-12-24 14:36:39 +0800

    add test commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Button from '@material-ui/core/Button';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
-
-function App() {
-  return (
-    <Button variant="contained" color="primary">
-      你好，世界
-    </Button>
-  );
+function App(){
+  return (<div> <h1>Herllo World</h1></div>);
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+ReactDOM.render(<App/>, document.querySelector('#root'));
